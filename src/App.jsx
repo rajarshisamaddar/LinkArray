edd26928b4501face8fd590709ea242853513789
Rajarshi Samaddar <itsrajarshi@hotmail.com>
2023-10-09 09:57:27 +0530

    update: init ✅

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="text-amber-300">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
