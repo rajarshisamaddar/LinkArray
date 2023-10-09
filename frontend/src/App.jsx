bfd069e34755eb3db9ff3b834b1441a4fd1b5dad
soumayadip2002 <soumayadipsaha2002@gmail.com>
2023-10-09 13:11:18 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
-import Navbar from "./components/Navbar/Navbar";
-import { BrowserRouter } from "react-router-dom";
-import Mobile from "./components/Mobile/Mobile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <>
-
+      <Routes>
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Routes>
     </>
   );
 }
